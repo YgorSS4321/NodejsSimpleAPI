911b6f390fd5eb4284bd8ef9de3c4565bf0c9fae
YgorSS4321 <yss5@aluno.ifal.edu.br>
2023-03-26 17:16:04 -0300

    completing the controllers

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest } from "fastify";
 import { PrismaClient } from "@prisma/client";
 import { z } from 'zod';
+import { equal } from "assert";
 
 export const prisma = new PrismaClient({
     log: ['query'],
@@ -10,7 +11,6 @@ export const prisma = new PrismaClient({
 
 
 export async function defaultReturn(request: FastifyRequest){
-
     return {
         name: "teste",
         someNumber: "1234",
@@ -38,18 +38,13 @@ export async function showPostalCards(request: FastifyRequest){
 }
 
 export async function showPostalCardById(request: FastifyRequest){
-   const validateID = z.object({
-    id: z.string().transform(id => Number(id))
-   })
+   const validateID = z.object({id: z.string().transform(id => Number(id))});
 
    const { id } = validateID.parse(request.params);
    
-   return await prisma.postCard.findFirst({
+   return await prisma.postCard.findUnique({
         where: {
-            id: {
-                equals: id,
-                
-            },
+            id: id,
         },
         select: {
             title: true,
@@ -71,8 +66,15 @@ export async function registerPostCard(request: FastifyRequest){
     const getPostalCardsSchema = z.object({
         title: z.string().min(2).max(100),
         description: z.string().max(600),
-        image_url: z.string().url().nullable(),
-        created_at: z.string().datetime().nullable()
+        image_url: z.string().url().nullable().optional(),
+        created_at: z.string().datetime().nullable().optional(),
+        selos: z.array(
+            z.object({
+                title: z.string().min(2).max(100),
+                origin_place: z.string().max(200),
+                tax: z.number().nonnegative()
+            })
+        ).optional()
     });
 
     const {title, description} = getPostalCardsSchema.parse(request.body);
@@ -90,6 +92,47 @@ export async function registerPostCard(request: FastifyRequest){
     
 }
 
-export async function deletePostCard(request: FastifyRequest){
+export async function deletePostCardById(request: FastifyRequest){
+
+    const validateID = z.object({
+        id: z.string().transform(id => Number(id)).optional()
+    })
+    
+    const { id } = validateID.parse(request.params);
+
+    await prisma.postCard.delete({
+        where: {
+            id: id
+        },
+    })
+
+
+}
+
+export async function updateSeloPostalTax(request: FastifyRequest){
+    
+    const validateUpdateSchema = z.object({
+        id: z.string().transform(id => Number(id)).optional(),
+        tax: z.number().nonnegative()
+    })
+
+    type UpdateTaxSchemaInput = z.input<typeof validateUpdateSchema>
+    type UpdateTaxSchemaOutput = z.output<typeof validateUpdateSchema>
+    
+    
+    //request.body
+
+    const {id, tax} = validateUpdateSchema.parse(request.body)
+
+
+    await prisma.seloPostal.update({
+        where: {
+            id: id,
+        },
+        data: {
+            tax: tax,
+        }
+    })
+
 
 }
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,11 +1,12 @@
 import { FastifyInstance } from "fastify";
-import { defaultReturn, deletePostCard, registerPostCard, showPostalCardById, showPostalCards } from "../controller/controller";
+import { defaultReturn, deletePostCardById, registerPostCard, showPostalCardById, showPostalCards, updateSeloPostalTax } from "../controller/controller";
 
 export async function appRoutes(app: FastifyInstance){
     app.get("/", defaultReturn);
     app.get("/postcards", showPostalCards);
     app.get("/postcards/:id", showPostalCardById);
     app.post("/postcards/new", registerPostCard);
-    app.delete("/postcards/:id/delete", deletePostCard);
+    app.delete("/postcards/:id/delete", deletePostCardById);
+    app.patch("/postcards/update", updateSeloPostalTax);
 
 }
